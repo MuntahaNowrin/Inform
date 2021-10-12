ce74dea886163009b29a4d7c67fb27c6f2545263
MuntahaNowrin <muntahanowrin11@gmail.com>
2021-10-12 22:30:09 +0200

    new dependencies added to the pom.xml

--- src/test/java/Selenium/Inform/AppTest.java
+++ src/test/java/Selenium/Inform/AppTest.java
@@ -4,9 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest {
 	private static WebDriver driver = null;
 
