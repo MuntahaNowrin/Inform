fbee4afbb9b364300620697ab91abd3dc3e8ed41
MuntahaNowrin <muntahanowrin11@gmail.com>
2021-10-12 21:27:37 +0200

    new test cases added

--- src/test/java/Selenium/Inform/AppTest.java
+++ src/test/java/Selenium/Inform/AppTest.java
@@ -1,38 +1,36 @@
 package Selenium.Inform;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
+	private static WebDriver driver = null;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	public static void main(String[] args) {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+		String projectpath = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver", projectpath + "/lib/chromedriver/chromedriver");
+		driver = new ChromeDriver();
+
+		driver.get("http://localhost:8085/java-tomcat-maven-example/");
+
+		String bodyText = driver.findElement(By.tagName("h2")).getText();
+		System.out.println(bodyText);
+
+		if (bodyText.contains("Hello World!")) {
+
+			System.out.println("TEST PASSED!");
+
+		} else {
+
+			System.out.println("TEST FAILED");
+		}
+
+		driver.close();
+
+	}
 }
